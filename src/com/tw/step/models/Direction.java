4e0cbfefcddce8d73f7db763d7fc54937bbb9f66
Beauti <183709979+beautisui@users.noreply.github.com>
2025-05-11 15:28:01 +0530

    Modifies direction enum class

--- src/com/tw/step/models/Direction.java
+++ src/com/tw/step/models/Direction.java
@@ -1,51 +1,35 @@
 package com.tw.step.models;
 
 public enum Direction {
-    EAST, WEST, NORTH, SOUTH;
+    NORTH, EAST, SOUTH, WEST;
 
     public Direction turnLeft() {
         switch (this) {
-            case NORTH:
-                return WEST;
-            case EAST:
-                return NORTH;
-            case SOUTH:
-                return EAST;
-            case WEST:
-                return SOUTH;
+            case NORTH: return WEST;
+            case EAST: return NORTH;
+            case SOUTH: return EAST;
+            case WEST: return SOUTH;
+            default: throw new IllegalStateException("Unexpected value: " + this);
         }
-        return this;
     }
-
 
     public Direction turnRight() {
         switch (this) {
-            case NORTH:
-                return EAST;
-            case EAST:
-                return SOUTH;
-            case SOUTH:
-                return WEST;
-            case WEST:
-                return NORTH;
+            case NORTH: return EAST;
+            case EAST: return SOUTH;
+            case SOUTH: return WEST;
+            case WEST: return NORTH;
+            default: throw new IllegalStateException("Unexpected value: " + this);
         }
-        return this;
     }
-
 
     public Position move(Position position) {
         switch (this) {
-            case NORTH:
-                return new Position(position.getX(), position.getY() + 1);
-            case SOUTH:
-                return new Position(position.getX(), position.getY() - 1);
-            case EAST:
-                return new Position(position.getX() + 1, position.getY());
-            case WEST:
-                return new Position(position.getX() - 1, position.getY());
-            default:
-                throw new IllegalStateException("Unexpected value: " + this);
+            case NORTH: return new Position(position.getX(), position.getY() + 1);
+            case SOUTH: return new Position(position.getX(), position.getY() - 1);
+            case EAST: return new Position(position.getX() + 1, position.getY());
+            case WEST: return new Position(position.getX() - 1, position.getY());
+            default: throw new IllegalStateException("Unexpected value: " + this);
         }
     }
-
 }
