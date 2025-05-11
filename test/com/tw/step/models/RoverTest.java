5d513ec41f25828b780f31fefbe4196e7a3f1107
Beauti <183709979+beautisui@users.noreply.github.com>
2025-05-11 15:26:58 +0530

    Adds test to process the instruction in rover

--- test/com/tw/step/models/RoverTest.java
+++ test/com/tw/step/models/RoverTest.java
@@ -10,43 +10,55 @@ class RoverTest {
     void shouldTurnLeftFromNorth() {
         Rover rover = new Rover(0, 0, Direction.NORTH);
         rover.turnLeft();
-        assertEquals(WEST, rover.getDirection());
+        assertEquals(Direction.WEST, rover.getDirection());
     }
 
     @Test
-    void shouldTurnRightToEast() {
+    void shouldTurnRightFromNorth() {
         Rover rover = new Rover(0, 0, Direction.NORTH);
         rover.turnRight();
-        assertEquals(EAST, rover.getDirection());
-    }
-
-    @Test
-    void shouldTurnRightFromWest() {
-        Rover rover = new Rover(0, 0, Direction.WEST);
-        rover.turnRight();
-        assertEquals(NORTH, rover.getDirection());
-    }
-
-    @Test
-    void shouldTurnRightFromEast() {
-        Rover rover = new Rover(0, 0, Direction.EAST);
-        rover.turnRight();
-        assertEquals(SOUTH, rover.getDirection());
+        assertEquals(Direction.EAST, rover.getDirection());
     }
 
     @Test
     void shouldMoveForwardWhenFacingNorth() {
-        Rover rover = new Rover(0, 0, NORTH);
+        Rover rover = new Rover(0, 0, Direction.NORTH);
         rover.moveForward();
         assertEquals(0, rover.getX());
         assertEquals(1, rover.getY());
     }
 
     @Test
+    void shouldMoveForwardWhenFacingEast() {
+        Rover rover = new Rover(0, 0, Direction.EAST);
+        rover.moveForward();
+        assertEquals(1, rover.getX());
+        assertEquals(0, rover.getY());
+    }
+
+    @Test
     void shouldMoveForwardWhenFacingSouth() {
-        Rover rover = new Rover(0, 1, SOUTH);
+        Rover rover = new Rover(0, 1, Direction.SOUTH);
         rover.moveForward();
         assertEquals(0, rover.getX());
         assertEquals(0, rover.getY());
+    }
+
+    @Test
+    void shouldMoveForwardWhenFacingWest() {
+        Rover rover = new Rover(1, 0, Direction.WEST);
+        rover.moveForward();
+        assertEquals(0, rover.getX());
+        assertEquals(0, rover.getY());
+    }
+
+
+    @Test
+    void shouldFollowInstructions() {
+        Rover rover = new Rover(1, 2, Direction.NORTH);
+        rover.processInstructions("LMLMLMLMM");
+        assertEquals(1, rover.getX());
+        assertEquals(3, rover.getY());
+        assertEquals(Direction.NORTH, rover.getDirection());
     }
 }
