5852bde2a07862b3fca6764ea87343491d187a49
Beauti <183709979+beautisui@users.noreply.github.com>
2025-05-11 15:32:28 +0530

    Implemets Rovers can move according to the instruction which are hard coded value

--- src/com/tw/step/Main.java
+++ src/com/tw/step/Main.java
@@ -1,4 +1,16 @@
 package com.tw.step;
 
+import com.tw.step.models.Direction;
+import com.tw.step.models.Rover;
+
 public class Main {
+    public static void main(String[] args) {
+        Direction direction = Direction.valueOf("NORTH");
+
+        Rover rover = new Rover(1, 2, direction);
+        rover.processInstructions("LMLMLMLMM");
+
+        System.out.println(rover.getX() + " " + rover.getY() + " " + rover.getDirection());
+    }
 }
+
